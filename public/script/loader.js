3ccdd76329de5962bd66c576bb6ba929ad52de3d
NajioMad <najiomad@gmail.com>
2023-02-22 05:30:36 +0000

    update

---
+++ public/script/loader.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    let animation = '<div id="loader-wrapper">';
+        animation += '<div id="loader"></div>';
+        animation += '<div class="loader-section section-left"></div>';
+        animation += '<div class="loader-section section-right"></div>';
+        animation += '</div>';
+    $("#animation-box").append(animation);
+});
